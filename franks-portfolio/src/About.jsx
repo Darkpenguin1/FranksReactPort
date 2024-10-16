604570ff084fb333978111911d774e299270bc6c
Darkpenguin1 <Frank.E.Peraza@outlook.com>
2024-10-15 22:15:21 -0400

    Working on the design layout of the about sect

--- franks-portfolio/src/About.jsx
+++ franks-portfolio/src/About.jsx
@@ -1,19 +1,21 @@
 // About.jsx
 function About() {
     return (
-      <section className="bg-white text-gray-900 py-20 px-4" id="about">
+      <section className="bg-white text-gray-900 py-24 px-6" id="about">
         <div className="max-w-4xl mx-auto text-center">
-          <h2 className="text-4xl font-bold mb-4">About Me</h2>
-          <p className="text-lg mb-6">
-            I’m a software engineering student passionate about coding and web development.
-          </p>
+          <h2 className="text-4xl font-bold mb-4">Who am I?</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="flex justify-center">
+              <img src="/penguinLogoPicture.png" alt="Penguin Logo" className=" h-16 w-16"/>
+            </div>
+            <p className="text-lg mb-6">
+              I'm an 18 year old junior at Unc Charlotte studying computer science.
+              I'm an aspiring Software/Cloud Engineer!
+            </p>
+          </div>
           
-          <a
-            href="#"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            View Resume
-          </a>
+          
+
         </div>
       </section>
     );
